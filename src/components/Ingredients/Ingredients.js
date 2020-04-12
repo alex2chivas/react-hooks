f9b2ce356d29974d9358f48adcd6dbc571a9174a
Alexis Flores <alex2chivas@hotmail.com>
2020-04-12 00:19:13 -0700

    added fetch calls

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,11 +7,37 @@ import Search from './Search';
 const Ingredients = () => {
 	const [ userIngredients, setUserIngredients ] = useState([]);
 
+	useEffect(() => {
+		fetch('https://ingredients-using-hooks.firebaseio.com/ingredients.json')
+			.then(response => response.json())
+			.then(responseData => {
+				const loadIngredients = [];
+				for (const key in responseData) {
+					loadIngredients.push({
+						id: key,
+						title: responseData[key].title,
+						amount: responseData[key].amount
+					});
+				}
+				setUserIngredients(loadIngredients);
+			});
+	}, []);
+
 	const addIngredientHandle = ingredient => {
-		setUserIngredients(prevIngredients => [
-			...prevIngredients,
-			{ id: Math.floor(Math.random() * 100).toString(), ...ingredient }
-		]);
+		fetch('https://ingredients-using-hooks.firebaseio.com/ingredients.json', {
+			method: 'Post',
+			body: JSON.stringify(ingredient),
+			headers: { 'Content-Type': 'application/json' }
+		})
+			.then(response => {
+				return response.json();
+			})
+			.then(responseData => {
+				setUserIngredients(prevIngredients => [
+					...prevIngredients,
+					{ id: responseData.name, ...ingredient }
+				]);
+			});
 	};
 
 	const onRemoveIngredient = ingredientId => {
