efe87d639d326ed4518ce97cba3436b47ebd1d68
Alexis Flores <alex2chivas@hotmail.com>
2020-04-11 19:25:38 -0700

    added the get request from axios

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState , useEffect} from 'react';
 
 import './IngredientList.css';
+import ingredientsApiCall from '../../api/ingredients'
 
 const IngredientList = props => {
+  const [ingredients, setIngredients] = useState([])
+
+  const fetchLists = async () => {
+    const response = await ingredientsApiCall.get('/ingredients')
+    setIngredients(response.data)
+  }
+
+  useEffect(() => {
+    fetchLists()
+  }, [props.ingredients]);
+
   return (
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
-        {props.ingredients.map(ig => (
+        {ingredients.map(ig => (
           <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
             <span>{ig.title}</span>
             <span>{ig.amount}x</span>
