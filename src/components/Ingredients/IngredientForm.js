eb4075149bf551a749ab4faa9be2b27c44fa3468
Alexis Flores <alex2chivas@hotmail.com>
2020-04-12 15:41:25 -0700

    removed the console.log

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -7,7 +7,6 @@ import './IngredientForm.css';
 const IngredientForm = React.memo(props => {
 	const [ enteredTitle, setEnteredTitle ] = useState('');
 	const [ enteredAmount, setEnteredAmount ] = useState('');
-	console.log('Rendering ingredient form');
 
 	const submitHandler = event => {
 		event.preventDefault();
