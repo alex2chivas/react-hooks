0304241813b44191e8f3561cd97f89a673ec622c
Alexis Flores <alex2chivas@hotmail.com>
2020-04-12 01:12:53 -0700

    added setTimers to send input if the user stoped typing after 1 second or less. Also, useEffect takes a return value that has to be a function which we did in this case to clear up the timer runs so we do not have them stacking ontop of each other

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,30 +6,39 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnterFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://ingredients-using-hooks.firebaseio.com/ingredients.json' + query)
-    .then(response => response.json())
-    .then(responseData => {
-      const loadIngredients = [];
-      for (const key in responseData) {
-        loadIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://ingredients-using-hooks.firebaseio.com/ingredients.json' + query)
+        .then(response => response.json())
+        .then(responseData => {
+          const loadIngredients = [];
+          for (const key in responseData) {
+            loadIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount
+            });
+          }
+          onLoadIngredients(loadIngredients)
         });
       }
-      onLoadIngredients(loadIngredients)
-    });
-  }, [enteredFilter, onLoadIngredients]);
+  }, 1000)
+
+  return () => {
+    clearTimeout(timer);
+  };
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilter} onChange={event => setEnterFilter(event.target.value)} />
+          <input ref={inputRef} type="text" value={enteredFilter} onChange={event => setEnterFilter(event.target.value)} />
         </div>
       </Card>
     </section>
