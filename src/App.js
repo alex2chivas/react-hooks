575c4b888cb86826adc832779b5f03c66f24bf76
Alexis Flores <alex2chivas@hotmail.com>
2020-04-12 17:48:36 -0700

    grabbed the finish code

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { useContext } from 'react';
 
 import Ingredients from './components/Ingredients/Ingredients';
 import Auth from './components/Auth';
-import { AuthContext } from './context/auth-content';
+import { AuthContext } from './context/auth-context';
 
 const App = props => {
-	const authContext = useContext(AuthContext);
+  const authContext = useContext(AuthContext);
 
-	let content = <Auth />;
-	if (authContext.isAuth) {
-		content = <Ingredients />;
-	}
+  let content = <Auth />;
+  if (authContext.isAuth) {
+    content = <Ingredients />;
+  }
 
-	return content;
+  return content;
 };
 
 export default App;
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -6,111 +6,93 @@ import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 import useHttp from '../../hooks/http';
 
-// Note - currentIngredients is actually state
 const ingredientReducer = (currentIngredients, action) => {
-	switch (action.type) {
-		case 'SET':
-			return action.ingredients;
-		case 'ADD':
-			return [ ...currentIngredients, action.ingredient ];
-		case 'DELETE':
-			return currentIngredients.filter(ing => ing.id !== action.id);
-		default:
-			throw new Error('Should not get there!');
-	}
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Should not get there!');
+  }
 };
 
 const Ingredients = () => {
-	const [ userIngredients, dispatch ] = useReducer(ingredientReducer, []);
-	const { isLoading, error, data, sendRequest, reqExtra, reqIdentifer } = useHttp();
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const {
+    isLoading,
+    error,
+    data,
+    sendRequest,
+    reqExtra,
+    reqIdentifer,
+    clear
+  } = useHttp();
 
-	// const [ userIngredients, setUserIngredients ] = useState([]);
-	// const [ isLoading, setIsloading ] = useState(false);
-	// const [ error, setError ] = useState();
+  useEffect(() => {
+    if (!isLoading && !error && reqIdentifer === 'REMOVE_INGREDIENT') {
+      dispatch({ type: 'DELETE', id: reqExtra });
+    } else if (!isLoading && !error && reqIdentifer === 'ADD_INGREDIENT') {
+      dispatch({
+        type: 'ADD',
+        ingredient: { id: data.name, ...reqExtra }
+      });
+    }
+  }, [data, reqExtra, reqIdentifer, isLoading, error]);
 
-	useEffect(
-		() => {
-			if (!isLoading && !error && reqIdentifer === 'REMOVE_INGREDIENT') {
-				dispatch({ type: 'DELETE', id: reqExtra });
-			} else if (!isLoading && !error && reqIdentifer === 'ADD_INGREDIENT') {
-				dispatch({ type: 'ADD', ingredient: { id: data.name, ...reqExtra } });
-			}
-		},
-		[ data, reqExtra, reqIdentifer ]
-	);
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
+  }, []);
 
-	const filteredIngredientHandler = useCallback(filteredIngredients => {
-		// setUserIngredients(filteredIngredients);
-		dispatch({ type: 'SET', ingredients: filteredIngredients });
-	}, []);
+  const addIngredientHandler = useCallback(ingredient => {
+    sendRequest(
+      'https://react-hooks-update.firebaseio.com/ingredients.json',
+      'POST',
+      JSON.stringify(ingredient),
+      ingredient,
+      'ADD_INGREDIENT'
+    );
+  }, [sendRequest]);
 
-	const addIngredientHandle = useCallback(ingredient => {
-		sendRequest(
-			'https://ingredients-using-hooks.firebaseio.com/ingredients.json',
-			'POST',
-			JSON.stringify(ingredient),
-			ingredient,
-			'ADD_INGREDIENT'
-		);
-		// dispatchHttp({ type: 'SEND' });
-		// fetch('https://ingredients-using-hooks.firebaseio.com/ingredients.json', {
-		// 	method: 'POST',
-		// 	body: JSON.stringify(ingredient),
-		// 	headers: { 'Content-Type': 'application/json' }
-		// })
-		// 	.then(response => {
-		// 		dispatchHttp({ type: 'RESPONSE' });
-		// 		return response.json();
-		// 	})
-		// 	.then(responseData => {
-		// 		// setUserIngredients(prevIngredients => [
-		// 		// 	...prevIngredients,
-		// 		// 	{ id: responseData.name, ...ingredient }
-		// 		// ]);
-		// 		dispatch({
-		// 			type: 'ADD',
-		// 			ingredient: { id: responseData.name, ...ingredient }
-		// 		});
-		// 	});
-	}, []);
+  const removeIngredientHandler = useCallback(
+    ingredientId => {
+      sendRequest(
+        `https://react-hooks-update.firebaseio.com/ingredients/${ingredientId}.json`,
+        'DELETE',
+        null,
+        ingredientId,
+        'REMOVE_INGREDIENT'
+      );
+    },
+    [sendRequest]
+  );
 
-	const onRemoveIngredient = useCallback(
-		ingredientId => {
-			sendRequest(
-				`https://ingredients-using-hooks.firebaseio.com/ingredients/${ingredientId}.json`,
-				'DELETE',
-				null,
-				ingredientId,
-				'REMOVE_INGREDIENT'
-			);
-			// return setUserIngredients(userIngredients.filter(ingredient => ingredient.id !== ingredientId));
-		},
-		[ sendRequest ]
-	);
+  const ingredientList = useMemo(() => {
+    return (
+      <IngredientList
+        ingredients={userIngredients}
+        onRemoveItem={removeIngredientHandler}
+      />
+    );
+  }, [userIngredients, removeIngredientHandler]);
 
-	const clearError = useCallback(() => {
-		// dispatchHttp({ type: 'CLEAR' });
-	}, []);
+  return (
+    <div className="App">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
 
-	const ingredientList = useMemo(
-		() => {
-			return <IngredientList ingredients={userIngredients} onRemoveItem={onRemoveIngredient} />;
-		},
-		[ userIngredients, onRemoveIngredient ]
-	);
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
-	return (
-		<div className='App'>
-			{error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
-
-			<IngredientForm onAddIngredient={addIngredientHandle} loading={isLoading} />
-
-			<section>
-				<Search onLoadIngredients={filteredIngredientHandler} />
-				{ingredientList}
-			</section>
-		</div>
-	);
+      <section>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        {ingredientList}
+      </section>
+    </div>
+  );
 };
 
 export default Ingredients;
